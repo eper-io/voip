da233fdd173e7209fbad336a577cfbf57993e878
Miklos Szegedi <hq@opensource.eper.io>
2023-08-09 13:41:24 -0700

    fix prompts

--- documentation/cicd.sh
+++ documentation/cicd.sh
@@ -16,28 +16,28 @@ date
 
 git status
 # Short glitch in the service. Acceptable.
-(git pull -r | grep 'up to date') || kill -9 `pgrep moosebroker`
+(git pull -r | grep 'up to date') || kill -9 `pgrep voipbroker`
 
 docker build -t line.eper.io/line /tmp/voip
 
 # We may want to run a privileged container in the future. It is difficult to mix podman and golang
 cd /tmp/voip
-go build -o /opt/moosebroker ./eos/main/main.go
+go build -o /opt/voipbroker ./eos/main/main.go
 
 # All the worker containers keep running for their respective customers until they shut down themselves. (~2 hours)
 
 # Run test container
 
-docker stop -t 2 moose
+docker stop -t 2 voiptest
 
 sleep 3
 
 # Running docker as root on the network is dangerous so we need to be very lean and careful with the codebase in /eos
 
-docker run -d --rm --name moose -e SITEURL=https://l.eper.io -e PORT=7777 -p 7777:443 -v /etc/letsencrypt/live/moose-assist.eper.io/fullchain.pem:/tmp/fullchain.pem:ro -v /etc/letsencrypt/live/moose-assist.eper.io/privkey.pem:/tmp/privkey.pem:ro line.eper.io/line
+docker run -d --rm --name voiptest -e SITEURL=https://l.eper.io -e PORT=7777 -p 7777:443 -v /etc/letsencrypt/live/moose-assist.eper.io/fullchain.pem:/tmp/fullchain.pem:ro -v /etc/letsencrypt/live/moose-assist.eper.io/privkey.pem:/tmp/privkey.pem:ro line.eper.io/line
 
 # Run the broker if needed.
 
-pgrep moosebroker || (DOCKERIMAGE=line.eper.io/line SITEURL=https://l.eper.io APIKEY=JVPSVWUIUTSXGPTWOVEWMHBUFJMVIALPQDMXQZROKZLYPYQGMBRQZMRWSQZIACQDKIFVWYQBWGGHQLGALYBQTAQNLHDR /opt/moosebroker >>/var/log/moosebroker)
+pgrep voipbroker || (DOCKERIMAGE=line.eper.io/line SITEURL=https://l.eper.io APIKEY=JVPSVWUIUTSXGPTWOVEWMHBUFJMVIALPQDMXQZROKZLYPYQGMBRQZMRWSQZIACQDKIFVWYQBWGGHQLGALYBQTAQNLHDR /opt/voipbroker >>/var/log/voipbroker)
 
 # WARNING blocking call. Do not extend here.
