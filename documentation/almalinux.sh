fe5e380a9a815aedadc7b8ec0c6173a69906e9ff
Miklos Szegedi <hq@opensource.eper.io>
2023-08-18 11:30:23 -0700

    robust launch script

--- documentation/almalinux.sh
+++ documentation/almalinux.sh
@@ -42,19 +42,41 @@ if ! [ -d /etc/letsencrypt/live/moose-assist.eper.io/ ]; then
   certbot --nginx -m hq@schmied.us --cert-name l.eper.io -d l.eper.io --https-port 4443 certonly
   cd /tmp
   git clone https://gitlab.com/eper.io/voip.git
+fi
 
 cat <<EOF >/tmp/voip/metadata/data.go
 package metadata
-// TODO Generate some random here
+
+// Licensed under Creative Commons CC0.
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+// You should have received a copy of the CC0 Public Domain Dedication along wi
+// If not, see <https:#creativecommons.org/publicdomain/zero/1.0/legalcode>.
+
+// data.go - Relay websocket configuration
+
+// You can set this as a default or load from os.Getenv("APIKEY")
 var ActivationKey = "QTOPZNNEYGPBKUQEKJYLHBIJVHAJEOOXWMFMXWJDDWNOIJVHXFXRLFJXAAOGEBRBUMQJEYYNDHBTWJUYVNEKZJMJTHHR"
 var SiteUrl = "https://l.eper.io"
 var Certificate = "/tmp/fullchain.pem"
 var PrivateKey = "/tmp/privkey.pem"
 var ContainerRuntime = ""
 
+// This requires a Certificate and PrivateKey in place
+// Prof I.: it is non-compliant and extremely dangerous to put tls keys to temp directory for bare metal servers.
+// Prof II.: temp directory gets cleaned up fast, so it is less likely to get scraped after container exit.
+// Prof III.: Do one thing, do it well. Make sure it does not leak, open and safely run it as root.
+// 2:1
+
+//var SiteUrl = "https://127.0.0.1:443"
+//var Certificate = "/tmp/fullchain.pem"
+//var PrivateKey = "/tmp/privkey.pem"
+
+// Use this for local unit testing
 var Info = ""
 var Bandwidth = ""
-var Silence = ""
 var RandomSalt = "XBGXTNTKIAVWBNHGODJGSSNUFBDISPRYVKCFLYBFHPEWBRHQHYUWQLHHOPZLDZREJIAVPGEQMHOJFICSXNWADFHIHFRR"
 
 EOF
@@ -67,7 +89,10 @@ RUN apt update; apt install -y docker-compose;
 # This will listen to tcp port metadata.Http11Port externally.
 CMD go run main.go
 EOF
-fi
+
+cd /tmp/voip/
+git config --global user.email "hq@schmied.us"
+git config --global user.name "eper.io installer"
 
 git add .
 git commit -m local
