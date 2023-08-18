a8b6d333176a6ac0624de1872ddca4b1746c258a
Miklos Szegedi <hq@opensource.eper.io>
2023-08-18 11:21:00 -0700

    robust launch script

--- documentation/almalinux.sh
+++ documentation/almalinux.sh
@@ -7,10 +7,13 @@
 # If not, see <https:#creativecommons.org/publicdomain/zero/1.0/legalcode>.
 
 # Installation:
-# while true; do timeout 600 /tmp/voip/documentation/almalinux.sh; done
-# Once set up a CI/CD from git:
-# nohup bash -c 'while true; do timeout 600 /tmp/voip/documentation/almalinux.sh; done' &
-# tail -f nohup.out
+# curl https://gitlab.com/eper.io/voip/-/raw/main/documentation/almalinux.sh?ref_type=heads >/tmp/setup.sh
+# Review code
+# bash /tmp/setup.sh
+
+# Once set up a CI/CD from git
+# Add this to 'crontab -e'
+# @reboot sudo /tmp/voip/documentation/bashrc.sh
 
 # Install moose-assist on AlmaLinux on Oracle Cloud
 touch /root/moose-assist || (echo *** run this as root ***; exit 1;)
@@ -36,15 +39,15 @@ ln -s /snap/bin/certbot /usr/bin/certbot
 
 if ! [ -d /etc/letsencrypt/live/moose-assist.eper.io/ ]; then
   echo *** SETUP NEEDED *** Replace all occurrences of moose-assist.eper.io in this file to your domain.
-  certbot --nginx -m hq@schmied.us --cert-name l.eper.io -d moose-assist.eper.io,l.eper.io --https-port 4443 certonly
+  certbot --nginx -m hq@schmied.us --cert-name l.eper.io -d l.eper.io --https-port 4443 certonly
   cd /tmp
   git clone https://gitlab.com/eper.io/voip.git
 
 cat <<EOF >/tmp/voip/metadata/data.go
 package metadata
 // TODO Generate some random here
-var ActivationKey = "QTOPZNNEYGPBKUQEKJYLHBIJVHAJEOOXVMFMXWJDDWNOIJVHXFXRLFJXAAOGEBRBUMQJEYYNDHBTWJUYVNEKZJMJTHHR"
-var SiteUrl = "https://moose-assist.eper.io"
+var ActivationKey = "QTOPZNNEYGPBKUQEKJYLHBIJVHAJEOOXWMFMXWJDDWNOIJVHXFXRLFJXAAOGEBRBUMQJEYYNDHBTWJUYVNEKZJMJTHHR"
+var SiteUrl = "https://l.eper.io"
 var Certificate = "/tmp/fullchain.pem"
 var PrivateKey = "/tmp/privkey.pem"
 var ContainerRuntime = ""
@@ -52,7 +55,7 @@ var ContainerRuntime = ""
 var Info = ""
 var Bandwidth = ""
 var Silence = ""
-var RandomSalt = "XBGXTNTKIAVWBNHGODJGSSNUFBDIYPRYVKCFLYBFHPEWBRHQHYUWQLHHOPZLDZREJIAVPGEQMHOJFICSXNWADFHIHFRR"
+var RandomSalt = "XBGXTNTKIAVWBNHGODJGSSNUFBDISPRYVKCFLYBFHPEWBRHQHYUWQLHHOPZLDZREJIAVPGEQMHOJFICSXNWADFHIHFRR"
 
 EOF
 
--- metadata/data.go
+++ metadata/data.go
@@ -20,6 +20,9 @@ var ContainerRuntime = ""
 // This requires a Certificate and PrivateKey in place
 // Prof I.: it is non-compliant and extremely dangerous to put tls keys to temp directory for bare metal servers.
 // Prof II.: temp directory gets cleaned up fast, so it is less likely to get scraped after container exit.
+// Prof III.: Do one thing, do it well. Make sure it does not leak, open and safely run it as root.
+// 2:1
+
 //var SiteUrl = "https://127.0.0.1:443"
 //var Certificate = "/tmp/fullchain.pem"
 //var PrivateKey = "/tmp/privkey.pem"
