4ba2f49a5f41aac7099dc4a5af820885929bc323
Miklos Szegedi <hq@opensource.eper.io>
2023-08-21 19:16:32 -0700

    ns

--- eos/data.go
+++ eos/data.go
@@ -20,7 +20,7 @@ const maxSessions = int64(200)
 
 // maxRuntime is the maximum time a node is available for launch requests
 const maxRuntime = 8 * time.Hour
-const InitialNodes = 3
+const InitialNodes = 2
 
 const BasePort = 49999
 const LastPort = 60000
