7f1b2751f2e905daffc3702d21d05af20ec7f2ab
Miklos Szegedi <hq@opensource.eper.io>
2023-08-30 20:22:15 -0700

    autoscale

--- documentation/cicd.sh
+++ documentation/cicd.sh
@@ -30,7 +30,7 @@ cd /tmp/voip
 # Running as root on the network is dangerous so we need to be very lean with the codebase in /eos
 # Run the broker if needed. It launches containers that do the call lines
 #pgrep voipbroker || (DOCKERIMAGE=line.eper.io/line SITEURL=https://l.eper.io APIKEY=JVPSVWUIUTSXGPTWOVEWMHBUFJMVIALPQDMXQZROKZLYPYQGMBRQZMRWSQZIACQDKIFVWYQBWGGHQLGALYBQTAQNLHDR nohup /opt/voipbroker >>/var/log/voipbroker &)
-(cat /var/log/voip | grep 'Your branch is ahead of') || kill -9 `pgrep voipbroker`
+(cat /var/log/voip | grep 'Successfully rebased and updated') || kill -9 `pgrep voipbroker`
 sleep 2
 
 export AUTOSCALE=no-proxy
